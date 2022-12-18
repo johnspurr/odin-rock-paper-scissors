37b80e371dc1f567bdc0d16786ba0b43beca25d8
John Spurr <79884239+johnspurr@users.noreply.github.com>
2022-12-17 23:21:46 -0500

    Implement score-keeping; add polish

--- script.js
+++ script.js
@@ -13,32 +13,64 @@ function playRound(playerSelection, computerSelection, score) {
 
     // Ties
     if (playerSelection === computerSelection) {
-        return `You tied! ${titleCase(playerSelection)} ties ${computerSelection}.`
+        return "tie"
     }
 
     // Wins
     if (playerSelection === "rock" && computerSelection === "scissors"
     || playerSelection === "scissors" && computerSelection === "paper"
     || playerSelection === "paper" && computerSelection === "rock") {
-        return `You won! ${titleCase(playerSelection)} beats ${computerSelection}.`
+        return "win"
     }
 
     // Losses
-    return `You lost! ${titleCase(computerSelection)} beats ${playerSelection}.`
+    return "lose"
 }
 
 function game(maxRounds = 5) {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    console.log("Welcome to 'Rock, Paper, Scissors', the game that separates the humans from the computers! Begin!")
+
     for (let round = 1; round <= maxRounds; round++) {
         let computerSelection = getComputerChoice();
-        var playerSelection;
+        let playerSelection;
 
         do {
-            playerSelection = prompt(`Round ${round}! Choose rock, paper, or scissors.`)
+            playerSelection = prompt(`Round ${round}! Choose rock, paper, or scissors:`)
         } while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors")
 
-        result = playRound(playerSelection, computerSelection)
-        console.log(`Round ${round}: ${result}`)
+        let result = playRound(playerSelection, computerSelection);
+        let resultStr;
+
+        if (result === "tie") {
+            resultStr = `You tied! ${titleCase(playerSelection)} ties ${computerSelection}.`
+        }
+        else if (result === "win") {
+            playerScore += 1;
+            resultStr = `You won! ${titleCase(playerSelection)} beats ${computerSelection}.`
+        }
+        else {
+            computerScore += 1
+            resultStr = `You lost! ${titleCase(computerSelection)} beats ${playerSelection}.`
+        }
+
+        console.log(`Round ${round} (${playerScore}-${computerScore}): ${resultStr}`)
     }
+
+    let finalStr;
+    if (playerScore == computerScore) {
+        finalStr = "It's a tie!"
+    }
+    else if (playerScore > computerScore) {
+        finalStr = "You win the game!"
+    }
+    else if (computerScore > playerScore) {
+        finalStr = "You lost the game!"
+    }
+    finalStr += ` The final score was ${playerScore}-${computerScore}.`
+    console.log(finalStr)
 }
 
 game();
